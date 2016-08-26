99d1a71106c5913e36a4c947d8f54c0435876108
apandey5 <apandey5@ncsu.github.edu>
2016-08-26 18:50:36 -0400

    final tests in main and screenshot

--- code/1/main.py
+++ code/1/main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
 
+import sys
+from utest import ok, oks
 import utest
 import who1
+import who2
 sys.dont_write_bytecode=True
 
-utest.oks()
+def add(a, b):
+	return (a + b)
+
+@ok
+def _test_add1():
+	'''Add two numbers'''
+	assert add(2, 3) == 5, "equality failure"
+
+oks()
