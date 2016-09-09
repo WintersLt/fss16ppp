a201bafeb42ca009ce0a6e49831ab66f5c9da6f3
abhishek_kr <abhishekk@inferess.com>
2016-09-09 18:29:11 -0400

    Add homework 3

---
+++ code/3/think10.py
@@ -0,0 +1,39 @@
+
+
+def has_duplicates(t):
+	newList = list(t)
+	newList.sort()
+	duplicateFlag = False
+	for i in range(1, len(newList)):
+		if (newList[i-1] == newList[i]):
+			duplicateFlag = True
+			break
+	return duplicateFlag
+
+
+nl = [1,6,2,3,9,6]
+print has_duplicates(nl)	
+#print nl
+
+import random
+
+def generateRandomBdayList():
+	bdayList = []
+	for i in range(23):
+		bday = random.randint(1,365)
+		bdayList.append(bday)
+	return bdayList
+
+
+
+simulationCount = 10000
+count =0
+for i in range(simulationCount):
+	bdayList = generateRandomBdayList()
+	hasDup = has_duplicates(bdayList)
+	if(hasDup == True):
+		count = count+1
+
+prob = count/float(simulationCount)
+print "prob = ", prob
+	
