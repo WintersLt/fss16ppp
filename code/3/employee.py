2a8b4e4c48cac0b15bbc2a66dde31872138c5668
abhishek_kr <abhishekk@inferess.com>
2016-09-09 19:03:37 -0400

    Add employee class

---
+++ code/3/employee.py
@@ -0,0 +1,20 @@
+class Employee:
+	''' An Employee class '''
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def __repr__(i):
+		return i.__class__.__name__+" " + i.name + " " + str(i.age)
+
+	def __lt__(self, other):
+		return self.age < other.age
+
+
+ram = Employee("ram ", 50)
+mohan = Employee(" mohan ",30)
+
+employees = [ram, mohan]
+print employees
+employees.sort()
+print employees
